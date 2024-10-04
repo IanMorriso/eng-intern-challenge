061001ceda45ca36620bac5f0de7ed085e28ead0
IanMorriso <112591354+IanMorriso@users.noreply.github.com>
2024-10-03 19:23:09 -0600

    program skeleton and braille char seperator

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,24 @@
 
+"""
+    Shopify Eng Intern Assignment: Braille Translator
+    Author: Ian Morrison
+    Date: October 3, 2024
+"""
+import re
+
+def main():
+    bSeperator(".....OO.....O.O...OO...........O.OOOO.....O.O...OO..........OO..OO.....OOO.OOOO..OOO")
+    return
+
+def bCharToEngl(braille):
+    return
+
+def eCharToBraille(english):
+    return
+
+def bSeperator(bText):
+    bList = re.findall('.{1,6}', bText)
+    print(bList)
+    return
+
+main()
